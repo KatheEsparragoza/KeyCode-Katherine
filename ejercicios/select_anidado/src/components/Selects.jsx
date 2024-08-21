a0175fd1ee27681aa9ff99053a301e327666ddc2
Katherine Esparragoza <caterinevillalba@gmail.com>
2024-08-21 14:10:58 -0500

    react ejercicio

---
+++ ejercicios/select_anidado/src/components/Selects.jsx
@@ -0,0 +1,11 @@
+import { SelectList } from "./components/SelectList";
+
+export const Selects = () => {
+  return (
+    <>
+      <SelectList title = "Estados"/>
+      <SelectList title = "Municipios"/>
+      <SelectList title = "Ciudades"/>
+    </>
+  );
+};
